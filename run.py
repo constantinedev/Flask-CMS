d4945e61b521ecf2f8011d86e69f6b44e5d06737
constantinedev <constantinedev@protonmail.com>
2023-02-07 23:07:15 +0000

    origan main

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from sqlite_utils.utils import sqlite3
 from datetime import datetime as DT , timezone as TZ
 from flask import Flask, request, make_response, Response, jsonify, redirect, url_for, render_template, flash, abort
 from flask_login import UserMixin, LoginManager, login_required, current_user, login_user, logout_user
+from flask_ckeditor import CKEditor, upload_success, upload_fail
 from werkzeug.middleware.proxy_fix import ProxyFix
 from system.setup import login
 from system.panel import set_password, check_password, panel_api, register, sing_in, sing_out, img_uploader
@@ -16,7 +17,10 @@ session.proxies['https'] = 'socks5h://localhost:9050'
 
 app = Flask(__name__)
 app.secret_key = b'82d52ae6cdbed60d2e6923b0b562f99adab07c6d6b57a55c5e2f4043ebde05d2'
+ckeditor = CKEditor(app)
 app.wsgi_app = ProxyFix(app.wsgi_app)
+app.config['CKEDITOR_PKG_TYPE'] = 'full-all'
+app.config['CKEDITOR_FILE_UPLOADER'] = 'uploads'
 
 if app.testing:
     app.config['WTF_CSRF_ENABLED'] = False
@@ -70,6 +74,22 @@ def user_conf(config):
     if config == 'update_profile':
       print('demo')
     return 'TEST Today' + str(DT.now())
+
+# @app.route('/files/<path:filename>')
+# def uploaded_files(filename):
+#     path = '/the/uploaded/directory'
+#     return send_from_directory(path, filename)
+
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     f = request.files.get('upload')
+#     # Add more validations here
+#     extension = f.filename.split('.')[-1].lower()
+#     if extension not in ['jpg', 'gif', 'png', 'jpeg']:
+#         return upload_fail(message='Image only!')
+#     f.save(os.path.join('/the/uploaded/directory', f.filename))
+#     url = url_for('uploaded_files', filename=f.filename)
+#     return upload_success(url, filename=f.filename)  # return upload_success call
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8001, debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,50 @@ $(document).ready(function () {
 
 	$("header").load('static/htm/header.html');
 	$("footer").load("static/htm/footer.html");
+	
+	tinymce.init({
+    selector: ".tinymce",
+    width: "100%",
+    min_height: 500,
+    plugins: [
+      "export advlist autolink link linkchecker image imagetools importcss lists charmap print preview hr anchor pagebreak spellchecker",
+      "searchreplace wordcount visualblocks visualchars advcode code fullscreen insertdatetime media mediaembed nonbreaking advtable",
+      "save table contextmenu directionality formatpainter pageembed template paste textcolor codesample quickbars powerpaste casechange textpattern",
+    ],
+    mobile: {
+      plugins:
+        "export advlist autolink link linkchecker image imagetools importcss lists charmap print preview hr anchor pagebreak spellchecker searchreplace wordcount visualblocks visualchars advcode code fullscreen insertdatetime media mediaembed nonbreaking advtable save table contextmenu directionality formatpainter pageembed template paste textcolor codesample quickbars powerpaste casechange textpattern",
+    },
+    imagetools_toolbar:
+      "rotateleft rotateright | flipv fliph | editimage imageoptions",
+    toolbar:
+      "print insertfile undo redo | styleselect | bold italic | code alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | export preview media fullpage | forecolor backcolor emoticons | codesample",
+    contextmenu:
+      "print template undo redo powerpaste | table inserttable | lists link image cell row column deletetable",
+    save_enablewhendirty: true,
+    file_picker_types: "file image media",
+    images_upload_url: "/panel?mod=imgupload",
+    images_file_types: "jpg,svg,png,jpge,webp",
+    paste_data_images: true,
+    automatic_uploads: true,
+    images_reuse_filename: false,
+    images_upload_base_path: "/static/uploads",
+    codesample_languages: [
+      { text: "HTML/XML", value: "markup" },
+      { text: "JavaScript", value: "javascript" },
+      { text: "CSS", value: "css" },
+      { text: "Processing", value: "processing" },
+      { text: "Python", value: "python" },
+    ],
+  });
+	document.addEventListener("focusin", (e) => {
+    if (
+      e.target.closest(
+        ".tox-tinymce-aux, .moxman-window, .tam-assetmanager-root"
+      ) !== null
+    ) {
+      e.stopImmediatePropagation();
+    }
+	});
+	
 });
