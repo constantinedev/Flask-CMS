4d0cb27a72afee50ee4b398cc6e2612ded0fd632
constantinedev <constantinedev@pm.me>
2023-09-06 21:02:07 +0100

    Create dashboard_api.py

---
+++ modules/plugins/dashboard/dashboard_api.py
@@ -0,0 +1,7 @@
+import re, io, os, sys, ast, json, sqlite_utils, logging, pgpy
+from datetime import datetime as DT, timezone as TZ, timedelta as TD
+from flask import Flask, Blueprint, request, make_response, Response, jsonify, redirect, url_for, render_template, flash, abort
+
+def page_loader():
+  if request.method == "GET":
+    return render_template('plugins/dashboard/dash00.htm', pag="dsahboard", title="Dashboard")
