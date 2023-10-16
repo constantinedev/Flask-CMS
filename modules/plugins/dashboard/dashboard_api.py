5eeb9c75d3ac98a11715f6303115d311543392a0
constantinedev <constantinedev@pm.me>
2023-10-16 05:43:39 +0100

    Update dashboard_api.py

--- modules/plugins/dashboard/dashboard_api.py
+++ modules/plugins/dashboard/dashboard_api.py
@@ -2,6 +2,10 @@ import re, io, os, sys, ast, json, sqlite_utils, logging, pgpy
 from datetime import datetime as DT, timezone as TZ, timedelta as TD
 from flask import Flask, Blueprint, request, make_response, Response, jsonify, redirect, url_for, render_template, flash, abort
 
-def page_loader():
+async def page_loader():
   if request.method == "GET":
-    return render_template('plugins/dashboard/main.htm', pag="dashboard", title="Dashboard")
+    pag = request.args.get('pag')
+    if pag == "" or pag=="dashboard":
+      return render_template('plugins/dashboard/main.htm', pag="dashboard", title="Dashboard")
+    else:
+      return render_template('plugins/dashboard/main.htm', pag=pag, title="Dashboard")
