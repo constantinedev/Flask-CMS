bebfb0b36096bec8870aea8485b71a4ef52fcc5e
Constantine Dev <constantinedev@protonmail.com>
2023-12-30 03:55:22 +0000

    Update asyncRequests.py

--- modules/asyncRequests.py
+++ modules/asyncRequests.py
@@ -1,5 +1,11 @@
 import re, os, io, sys, ast, ssl, csv, json, requests, sqlite_utils
 from datetime import datetime as DT, timezone as TZ, timedelta as TD
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.SAFARI.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=200)
+user_agent = user_agent_rotator.get_random_user_agent()
 
 session = requests.session()
 session.proxies = {}
