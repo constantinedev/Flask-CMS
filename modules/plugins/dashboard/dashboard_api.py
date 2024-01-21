edf06c310c1cc9434bae065d7344173fb8990592
constantinedev <constantinedev@pm.me>
2024-01-21 03:59:56 +0000

    Update dashboard_api.py

--- modules/plugins/dashboard/dashboard_api.py
+++ modules/plugins/dashboard/dashboard_api.py
@@ -3,18 +3,20 @@ from datetime import datetime as DT, timezone as TZ, timedelta as TD
 from sqlite_utils.utils import sqlite3
 from flask import Flask, Blueprint, request, make_response, Response, jsonify, redirect, url_for, render_template, flash, abort
 
-async def page_loader():
-  if request.method == "GET":
-    pag = request.args.get('pag')
-    if pag == "" or pag=="dashboard":
-      return render_template('plugins/dashboard/main.htm', pag="dashboard", title="Dashboard")
-    else:
-      return render_template('plugins/dashboard/main.htm', pag=pag, title="Dashboard")
+async def page_loader(page):
+	if request.method == "GET":
+		# page = request.args.get('pag')
+		if page=="dashboard":
+			return render_template('plugins/dashboard/main.htm', pag="dashboard", title="Dashboard")
+		elif page == "home" or page == "" or page is None:
+			return render_template('layout.html', pag='home', title="Dashboard")
+		else:
+			return render_template('layout.html', pag=page, title="Dashboard")
 
-  if request.method == "POST":
-    apis = request.args.get('apis')
-    if apis == "" or apis is None:
-      return redirect(url_for("/error_page"))
-    else:
-      retu_json = request.get_json()
-      return jsonify(retu_json), 200
+	if request.method == "POST":
+		apis = request.args.get('apis')
+		if apis == "" or apis is None:
+			return redirect(url_for("/error_page"))
+		else:
+			retu_json = request.get_json()
+			return jsonify(retu_json), 200
