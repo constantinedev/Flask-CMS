63072af104b31ed5a6919c55352ef0f2daa53b74
Constantine Dev <constantinedev@pm.me>
2024-07-16 10:16:56 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import re, os, io, sys, ast, ssl, csv, json, requests, sqlite_utils, base64, pytz, pgpy, pycountry, qrcode, qrcode.image.svg, logging
+import re, os, io, sys, ast, ssl, csv, json, base64, configparser, requests, sqlite_utils, pytz, pgpy, pycountry, qrcode, qrcode.image.svg, logging
 from datetime import datetime as DT , timezone as TZ, timedelta as TD
 from sqlite_utils.utils import sqlite3
 from flask import Flask, Blueprint, request, make_response, Response, jsonify, redirect, url_for, render_template, flash, abort, send_from_directory
