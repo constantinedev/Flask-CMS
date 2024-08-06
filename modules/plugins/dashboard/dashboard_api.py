d995041d9a2d36b1bceffc799f8e8d0b984abb42
Constantine Dev <constantinedev@pm.me>
2024-08-06 15:58:36 +0100

    Update dashboard_api.py

--- modules/plugins/dashboard/dashboard_api.py
+++ modules/plugins/dashboard/dashboard_api.py
@@ -4,24 +4,6 @@ from sqlite_utils.utils import sqlite3
 from flask import Flask, Blueprint, request, make_response, Response, jsonify, redirect, url_for, render_template, flash, abort
 from flask_login import current_user
 
-async def page_loader(page):
-	if request.method == "GET":
-		if page=="dashboard":
-			return await dashboard_panel()
-
-		elif page == "home" or page == "" or page is None:
-			return render_template('layout.html', pag='home', title="Dashboard")
-		else:
-			return redirect(f'/?pag={page}')
-
-	if request.method == "POST":
-		apis = request.args.get('apis')
-		if apis == "" or apis is None:
-			return redirect(url_for("/error_page"))
-		else:
-			retu_json = request.get_json()
-			return jsonify(retu_json), 200
-
 async def dashboard_panel():
 	if current_user.is_authenticated:
 		content = render_template('plugins/dashboard/main.htm', pag="dashboard", title="Dashboard")
