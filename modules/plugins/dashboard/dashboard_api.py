13fa0105588b02b3f79ecf6989e374138a87a24c
constantinedev <constantinedev@pm.me>
2024-08-08 01:46:21 +0100

    Update dashboard_api.py

--- modules/plugins/dashboard/dashboard_api.py
+++ modules/plugins/dashboard/dashboard_api.py
@@ -4,7 +4,7 @@ from sqlite_utils.utils import sqlite3
 from flask import Flask, Blueprint, request, make_response, Response, jsonify, redirect, url_for, render_template, flash, abort
 from flask_login import current_user
 
-async def dashboard_panel():
+async def dashboard_panel(page):
 	if current_user.is_authenticated:
 		content = render_template('plugins/dashboard/main.htm', pag="dashboard", title="Dashboard")
 		response = make_response(content)
