c56800d4d93b8f4baafef9e525541a794fc02fff
Constantine Dev <constantinedev@pm.me>
2024-12-09 20:48:55 +0000

    Update aioRequests.py

--- modules/aioRequests.py
+++ modules/aioRequests.py
@@ -1,10 +1,13 @@
-import re, os, io, sys, ast, ssl, csv, json, sqlite_utils, asyncio, aiohttp
+import re, os, io, sys, ast, ssl, csv, json, sqlite_utils, asyncio, aiohttp, base64
 from aiohttp_socks import ProxyType,ProxyConnector, ChainProxyConnector
 from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
-software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.SAFARI.value]
-operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.MAC.value]
-user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=200)
+from random_user_agent.params import SoftwareName, OperatingSystem, HardwareType, SoftwareEngine
+
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value, OperatingSystem.UNIX.value, OperatingSystem.MAC.value]
+hardware_types = [HardwareType.MOBILE.value, HardwareType.SERVER.value]
+software_names = [SoftwareName.CHROME.value, SoftwareName.FIREFOX.value, SoftwareName.SAFARI.value, SoftwareName.ANDROID.value]
+software_engines = [SoftwareEngine.GECKO.value, SoftwareEngine.WEBKIT.value, SoftwareEngine.BLINK.value]
+user_agent_rotator = UserAgent(software_names=software_names, software_engines=software_engines, hardware_types=hardware_types, operating_systems=operating_systems, limit=100)
 user_agent = user_agent_rotator.get_random_user_agent()
 
 async def gun_shell(url, method, type, headers, payload):
@@ -22,7 +25,7 @@ async def gun_shell(url, method, type, headers, payload):
     connector = ProxyConnector.from_url("socks5://127.0.0.1:9050")
     async with aiohttp.ClientSession(connector=connector) as session:
       return await bulletfire(session, url, method, type, headers, payload)
-     
+
 async def bulletfire(session, url, method, type, headers, payload):
   if method == "GET":
     async with session.get(url, headers=headers, data=payload) as response:
