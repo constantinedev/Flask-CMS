258afacc3c54410ded8dfe57fca9bfc203b4a20e
Constantine Dev <constantinedev@protonmail.com>
2025-03-07 21:21:45 +0000

    Update dashboard_api.py

--- modules/plugins/dashboard/dashboard_api.py
+++ modules/plugins/dashboard/dashboard_api.py
@@ -7,10 +7,10 @@ from flask_login import current_user
 from modules.cmsmod import pgpEnc, pgpDec, jwtMaker, jwtRecovery, tokMaker, tokRecovery, FX_2FA, CountryList, QueryFunction, svgQRmaker
 
 async def dashboard_panel(page):
+	info = request.args.get('info')
+	mod = request.args.get('mod')
+
 	if request.method == "GET":
-		info = request.args.get('info')
-		mod = request.args.get('mod')
-		
 		if current_user.is_authenticated:
 			if page == 'dashboard':
 				content = render_template('plugins/dashboard/main.htm', pag="dashboard", title="Dashboard")
@@ -22,7 +22,6 @@ async def dashboard_panel(page):
 
 	if request.method == "POST":
 		pag = request.args.get('pag')
-		info = request.args.get('info')
-		mod = request.args.get('mod')
+		
 		#####
 		### Function response on POST start here
